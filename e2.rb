a8db10761af5f3c3078d9cb6a23f0ce9bf2327aa
Corvo <doomleika@gmail.com>
2017-03-22 01:23:24 +0800

    e2

---
+++ e2.rb
@@ -0,0 +1,7 @@
+# comment 1
+puts "I could have code like this." # comment after first print(puts)
+# command after first puts
+puts "This will run."
+# comment after end
+
+puts #i comment this out "i don't get printed"
